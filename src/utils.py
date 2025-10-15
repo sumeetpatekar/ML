4273d5340d92bfda611a25fc73e71cf2d6e9c3d7
sumeet patekar <sumeetpatekarr>
2025-10-15 23:53:40 +0530

    7day

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,12 +20,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         model_report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
